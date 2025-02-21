bca061b048b87b8d74affd3eb8f8169a240e095c
carmeeb <ckbordado@gbox.adnu.edu.ph>
2025-02-21 12:56:53 +0800

    Add files via upload

---
+++ Bordado_prelims/README.md
@@ -0,0 +1,6 @@
+# Untitled
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/Carmela-Kaye-P-Bordado/pen/jENoyLL](https://codepen.io/Carmela-Kaye-P-Bordado/pen/jENoyLL).
+
---
+++ Bordado_prelims/src/script.js
@@ -0,0 +1,10 @@
+document.getElementById("getDog").addEventListener("click", async () => {
+    try {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        const data = await response.json();
+        document.getElementById("dogImage").src = data.message;
+    } catch (error) {
+        console.log("Error fetching dog image:", error);
+    }
+});
+
